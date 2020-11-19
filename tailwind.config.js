bb82c15a1322fc8adfa0e5471ee94c5d0764b1e3
Tommy Pratama <tommyhpratama@gmail.com>
2020-11-19 10:29:49 +0000

    Add config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
+    colors: {
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      emerald: colors.emerald,
+    },
     extend: {},
   },
   variants: {
